32684f34d37c929286daa0240c4a1e224ee1ebc1
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-05-28 15:36:44 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       session[:username] = params[:username]
       redirect_to '/'
     else
-      redirect_to '/'
+      redirect_to '/login'
     end
   end
 
