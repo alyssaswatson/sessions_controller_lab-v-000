959b44615c9db11836683f45450191d8721b6ded
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-05-28 15:32:10 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,9 @@ class SessionsController < ApplicationController
   def create
        session[:username] = params[:username]
        redirect_to '/'
-   end
+  end
+
+  def destroy
+    session.delete :username
+  end
 end
